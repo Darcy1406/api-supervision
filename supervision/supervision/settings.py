85ae599959e3689d447b49e229334dd943b7cdb8
Darcy140604 <darcyfranklin14@gmail.com>
2026-01-05 11:55:15 +0300

    feat : 25 - Mise a jour 25

--- supervision/supervision/settings.py
+++ supervision/supervision/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -238,6 +239,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# Dossier où collectstatic va mettre tous les fichiers statiques
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# URL pour accéder aux fichiers statiques
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
